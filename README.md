28f382771e4f33c6c93a7d5a82ca99cbc3ed3aee
Sandy Ciquero <sandy-ciqueroch@gmail.com>
2016-10-18 16:03:30 -0500

    inicio

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Versión 0.0.1 - 5
 Un elemento en el HTML con el mensaje "Añadir una lista", que al dar click muestre un input y un botón (formulario) para que el usuario ingrese el nombre de la lista.
 ![version1](http://i65.tinypic.com/2i1gkkg.png)
 ![version1.1](http://i64.tinypic.com/6j1tzl.png)
+Versión 0.0.2 - 5
+Mostrar en el HTML, el texto ingresado al dar click en el botón de "Guardar" del formulario (como si fuera título de la lista).
+Debajo del título, mostrar el mensaje clickeable de "Añadir una tarjea".
+![version2](http://i65.tinypic.com/2i1gkkg.png)
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var id = 1
 window.addEventListener("load", function(){
 	var boxContenedor = document.getElementById("boxContenedor");
 	var contenedor = document.getElementById("contenedor");
@@ -11,72 +12,100 @@ window.addEventListener("load", function(){
 		formulario.classList.remove("ocultar");
 		input.focus();
 	});
+  btnGuardar.addEventListener("click", function(e){
+    e.preventDefault();
+    formulario.classList.add("ocultar");
+  	lista.classList.remove("ocultar");
+     // lista.classList.add("aparece");
+    contenedor.classList.add("inicio");
+  	var titulo = document.createElement("span");
+  	var enlace = document.createElement("a");
+  	var contenido = input.value;
 
-}); 
-  // btnGuardar.addEventListener("click", function(e){
-  //   e.preventDefault();
-  //   formulario.classList.add("ocultar")
-  // 	// lista.classList.remove("ocultar");
-  // 	var titulo = document.createElement("span");
-  // 	var enlace = document.createElement("a");
-  // 	var contenido = input.value;
-
-  // 	titulo.setAttribute("id","titulo")
+  	titulo.setAttribute("id","titulo");
   		
-  // 	titulo.textContent = contenido;
-  // 	titulo.classList.add("fondo");
-  // 	enlace.classList.add("d-blk");
-  // 	enlace.textContent = "Añadir una Tarjeta";
-  // 	input.value = "";
+  	titulo.textContent = contenido;
+  	titulo.classList.add("fondo");
+  	enlace.classList.add("d-blk");
+  	enlace.textContent = "Añadir una Tarjeta";
+  	input.value = "";
 
-  //   enlace.setAttribute("href", "#");
+    enlace.setAttribute("href", "#");
 
-	 //  lista.parentElement.appendChild(titulo);
-	 //  lista.parentElement.appendChild(enlace);
+	  lista.parentElement.appendChild(titulo);
+	  lista.parentElement.appendChild(enlace);
   
-	 //  lista.setAttribute("id", "anadirTarjeta");
+	  lista.setAttribute("id", "tarjeta");
     
-  //  	var contenCard = document.createElement("div");
-	 //  contenCard.setAttribute("id","contenCard")
-	 //  enlace.parentElement.insertBefore(contenCard,enlace.parentElement.children[3]);
+   	var contenCard = document.createElement("div");
+    enlace.parentElement.insertBefore(contenCard,enlace.parentElement.children[3]);
+	  contenCard.setAttribute("id","contenCard");
 
-  //   nuevoContenedor();
+    contenCard.addEventListener("drop", drop);
+    contenCard.addEventListener("dragover", mover);
 
-  //   enlace.addEventListener("click", function(){
-  //     enlace.classList.add("ocultar");
+    function mover(e){
+      e.preventDefault();
+    } 
+    function drop(e){
+      e.preventDefault();
+      var data = e.dataTransfer.getData("text");
+      e.target.appendChild(document.getElementById(data));
+    }
 
-  //     var agregarForm = document.createElement("form");
-  //     var textArea = document.createElement("textarea");
-  //     var boton = document.createElement("button");
+    nuevoContenedor();
+
+    enlace.addEventListener("click", function(){
+      enlace.classList.add("ocultar");
+
+      var agregarForm = document.createElement("form");
+      var textArea = document.createElement("textarea");
+      var boton = document.createElement("button");
       
-  //     contenCard.appendChild(agregarForm);
-  //     agregarForm.appendChild(textArea);
-  //     agregarForm.appendChild(boton);
+      contenCard.appendChild(agregarForm);
+      agregarForm.appendChild(textArea);
+      agregarForm.appendChild(boton);
       
-  //     agregarForm.setAttribute("class","newForm");
-  //     textArea.setAttribute("class", "textArea");
-  //     boton.setAttribute("class", "boton");
-  //     textArea.focus();  
-  //     boton.textContent = "Guardar";
+      agregarForm.setAttribute("class","agregarForm");
+      textArea.setAttribute("class", "textArea");
+      boton.setAttribute("class", "boton");
+      textArea.focus();  
+      boton.textContent = "Guardar";
 
-  //     boton.addEventListener("click", function(e){
-  //       e.preventDefault();
-  //       agregarForm.classList.add("ocultar");
+      boton.addEventListener("click", function(e){
+        e.preventDefault();
+        agregarForm.classList.add("ocultar");
 
-  //       var tarjeta = document.createElement("span");
-  //       tarjeta.textContent = textArea.value;
-  //      	contenCard.appendChild(tarjeta);
-  //       tarjeta.setAttribute("class", "tarjeta");
-  //       enlace.classList.add("aparece");
+        var tarjeta = document.createElement("span");
+        tarjeta.textContent = textArea.value;
+       	contenCard.appendChild(tarjeta);
+        tarjeta.setAttribute("class", "tarjeta");
+        tarjeta.setAttribute("draggable","true");
+        tarjeta.setAttribute("id",id++);
 
-  //     });
-  //   });
-  // });
-  // function nuevoContenedor(){
-  //   var inicio = document.createElement("div");
-  //   boxContenedor.appendChild(inicio);
-  //   inicio.setAttribute("class", "inicio");
-  //   inicio.appendChild(lista);
-  //   inicio.appendChild(formulario);
-  //   lista.classList.add("aparece");
-  // }
+        tarjeta.addEventListener("dragstart", arrastrar);
+
+        function arrastrar(e){
+          e.dataTransfer.setData("text",e.target.id);
+        }
+
+        enlace.classList.remove("ocultar");
+
+      });
+    });
+  });
+  function nuevoContenedor(){
+    var inicio = document.createElement("div");
+    boxContenedor.appendChild(inicio);
+    inicio.setAttribute("class", "inicio");
+    inicio.appendChild(lista);
+    inicio.appendChild(formulario);
+    // lista.classList.add("aparece");
+    lista.classList.remove("ocultar");
+  }
+ 
+}); 
+
+  
+
+
